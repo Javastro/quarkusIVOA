a87e6e004268712b7c625c37fb1bee991bcbd1f5
Paul Harrison <paul.harrison@manchester.ac.uk>
2026-08-25 12:04:05 +0100

    move from snapshot dependedncies

--- quarkus-tap-lib/build.gradle.kts
+++ quarkus-tap-lib/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 
 dependencies {
     implementation(platform("org.javastro:bom:2026.2"))
-    api("org.javastro.ivoa.core:tap:0.1.0-SNAPSHOT")
-    implementation("org.javastro.ivoa.core:common:0.1.0-SNAPSHOT")
+    api("org.javastro.ivoa.core:tap:0.9.0")
+    implementation("org.javastro.ivoa.core:common:0.9.0")
     implementation("io.quarkus:quarkus-rest")
     implementation("org.jspecify:jspecify:1.0.0")
 
